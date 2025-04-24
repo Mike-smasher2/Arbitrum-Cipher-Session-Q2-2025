f140e769800f7d6d6dcb01bd7e1a7e48bd80334f
dubzz <victorjustice2003@gmail.com>
2025-04-24 09:13:07 +0100

    Add week-1 task

--- week-1/dubem/Calculator.sol
+++ week-1/dubem/Calculator.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity 0.8.26;
 
 contract Calculator{
     uint256 public Result;
 
   //This is a fuction to perform addition//
     function add(uint256 a, uint256 b) public pure returns(uint256){
-        returns a + b;
+        return a + b;
     }
 
 //this is a fuction to subtract values//
     function subtract(uint256 a , uint256 b) public pure returns(uint256){
         require(b <= a, "cannot subtract");
-        returns a - b;
+        return a - b;
     }
 
  //This is a function to multiply values//
@@ -26,13 +26,11 @@ contract Calculator{
 
     }
  //this is a fuction that stores the results of values gotten//
-    function storeResult(_inputresult) public {
-        Result = _inputresult;
+    function storeResult(uint256 _inputResult) public {
+        Result = _inputResult;
     }
 
 //this is a function to get the last updated result stored//
-    function getLastResult() public view {
-        return Result;
-    }
-
+    function getLastResult() public view returns(uint){        
+        return Result; }
 }
